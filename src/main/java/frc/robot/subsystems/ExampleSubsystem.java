3440482e8bd3065d7f1dfc33bdbf1b8fbc64883b
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2022-05-12 18:49:21 -0400

    DrivtrainSub

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -8,8 +8,23 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  private final WPI_VictorSPX myVictor1;
+  private final WPI_VictorSPX myVictor2;
+  private final WPI_VictorSPX myVictor3;
+  private final WPI_VictorSPX myVictor4;
+  private final MotorControllerGroup groupleft;
+  private final MotorControllerGroup groupright;
+  DifferentialDrive m_robotDrive;
+
+  public ExampleSubsystem() {
+    this.myVictor1 = new WPI_VictorSPX(1);
+    this.myVictor2 = new WPI_VictorSPX(2);
+    this.myVictor3 = new WPI_VictorSPX(3);
+    this.myVictor4 = new WPI_VictorSPX(4);
+    this.groupleft = new MotorControllerGroup(myVictor1, myVictor2);
+    this.groupright = new MotorControllerGroup(myVictor3, myVictor4);
+    this.m_robotDrive = new DifferentialDrive(groupright, groupleft);
+  }
 
   @Override
   public void periodic() {
