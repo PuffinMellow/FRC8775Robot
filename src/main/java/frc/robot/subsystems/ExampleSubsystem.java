699beca4314a1214e2b86e711703f541a7f3072b
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2022-05-12 19:19:04 -0400

    Update 7:18PM 5/12/2022

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -5,9 +5,12 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class Drivetrain extends SubsystemBase {
+  //Robot objects
   private final WPI_VictorSPX myVictor1;
   private final WPI_VictorSPX myVictor2;
   private final WPI_VictorSPX myVictor3;
@@ -16,11 +19,17 @@ public class ExampleSubsystem extends SubsystemBase {
   private final MotorControllerGroup groupright;
   DifferentialDrive m_robotDrive;
 
+  //Driving varibles
+  private float drivingTurnRate = 0;
+  private float drivingSpeed = 0;
+
+  //Sensor input varibles
+
   public ExampleSubsystem() {
-    this.myVictor1 = new WPI_VictorSPX(1);
-    this.myVictor2 = new WPI_VictorSPX(2);
-    this.myVictor3 = new WPI_VictorSPX(3);
-    this.myVictor4 = new WPI_VictorSPX(4);
+    this.myVictor1 = new WPI_VictorSPX(Constants.drivetrain1);
+    this.myVictor2 = new WPI_VictorSPX(Constants.drivetrain2);
+    this.myVictor3 = new WPI_VictorSPX(Constants.drivetrain3);
+    this.myVictor4 = new WPI_VictorSPX(Constants.drivetrain4);
     this.groupleft = new MotorControllerGroup(myVictor1, myVictor2);
     this.groupright = new MotorControllerGroup(myVictor3, myVictor4);
     this.m_robotDrive = new DifferentialDrive(groupright, groupleft);
@@ -30,6 +39,7 @@ public class ExampleSubsystem extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
 
   @Override
   public void simulationPeriodic() {
