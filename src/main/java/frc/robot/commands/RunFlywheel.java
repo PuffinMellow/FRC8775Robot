0364171356d9fe75202c14f90d5e51ff7794df2a
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2022-05-16 18:47:08 -0400

    5/16/2022 Commit

--- src/main/java/frc/robot/commands/RunFlywheel.java
+++ src/main/java/frc/robot/commands/RunFlywheel.java
@@ -1,33 +1,32 @@
-
 package frc.robot.commands;
 
-import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class RunIntake extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final Intake drivetrainSubsystem;
+  private final Shooter shooterSubsystem;
   private double stopTime;
-  private double intakeSpeed;
+  private double RPM;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public RunIntake(Intake subsystem, double maxTime, double intakeSpeed) {
+  public RunIntake(Shooter subsystem, double maxTime, double rpm) {
     this.stopTime = System.getTimeMillis() + maxTime;
-    this.intakeSubsystem = subsystem;
-    this.intakeSpeed = intakeSpeed;
+    this.shooterSubsystem = subsystem;
+    this.RPM = RPM;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(this.intakeSubsystem);
+    addRequirements(this.shooterSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-      this.intakeSubsystem.setIntakeSpeed(this.intakeSpeed);
+      this.shooterSubsystem.setRPM(this.RPM);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -37,7 +36,7 @@ public class RunIntake extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-      this.intakeSubsystem.setIntakeSpeed(0);
+      this.shooterSubsystem.setRPM(0);
   }
 
   // Returns true when the command should end.
