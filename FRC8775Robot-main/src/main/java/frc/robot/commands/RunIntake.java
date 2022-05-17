ef68a72d193cabf9058def8a40af7bcef576b219
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2022-05-17 08:05:51 -0400

    Add files via upload

---
+++ FRC8775Robot-main/src/main/java/frc/robot/commands/RunIntake.java
@@ -0,0 +1,52 @@
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class RunIntake extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Intake drivetrainSubsystem;
+  private double stopTime;
+  private double intakeSpeed;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public RunIntake(Intake subsystem, double maxTime, double intakeSpeed) {
+    this.stopTime = System.getTimeMillis() + maxTime;
+    this.intakeSubsystem = subsystem;
+    this.intakeSpeed = intakeSpeed;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(this.intakeSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+      this.intakeSubsystem.setIntakeSpeed(this.intakeSpeed);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+      this.intakeSubsystem.setIntakeSpeed(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if(System.getTimeMillis() > this.stopTime) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
